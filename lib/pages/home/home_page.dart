88b6b458b4c19ff61446e3832d4c769cb9dcf330
01fc <javier.francisco01.09@gmail.com>
2021-11-14 19:55:45 -0600

    QR scanner agregado: pantalla scanner que muestra banner con datos del QR modificado: home page ahora tiene solo 3 bottom items y 1 action en appbar agregado: home page tiene metodo que retorna conenido escaneado de QR

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -16,21 +16,42 @@ class _HomePageState extends State<HomePage> {
   final _pageNameList = [
     "Google books",
     "My library",
-    "Search by QR",
+    // "Search by QR",
     "About",
   ];
+
   final _pageList = [
     FreeBooks(),
     MyLibrary(),
-    QRSearch(),
+    // QRSearch(),
     AboutApp(),
   ];
+
+  var _pageActions = [
+    <Widget>[],
+    <Widget>[],
+    //<Widget>[],
+    <Widget>[],
+  ];
+
+  @override
+  void initState() {
+    // my library action
+    _pageActions[1].add(
+      IconButton(
+        onPressed: _openQrScannerPage,
+        icon: Icon(Icons.qr_code_scanner),
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("${_pageNameList[_currentPageIndex]}"),
+        actions: _pageActions[_currentPageIndex],
       ),
       body: IndexedStack(
         index: _currentPageIndex,
@@ -55,16 +76,31 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(Icons.library_books),
             label: "${_pageNameList[1]}",
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code_scanner),
-            label: "${_pageNameList[2]}",
-          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.qr_code_scanner),
+          //   label: "${_pageNameList[2]}",
+          // ),
           BottomNavigationBarItem(
             icon: Icon(Icons.info),
-            label: "${_pageNameList[3]}",
+            label: "${_pageNameList[2]}",
           ),
         ],
       ),
+    );
+  }
+
+  void _openQrScannerPage() async {
+    Navigator.of(context)
+        .push(
+      MaterialPageRoute(
+        builder: (context) => QRSearch(),
+      ),
+    )
+        .then(
+      (qrValue) {
+        print(qrValue);
+        //
+      },
     );
   }
 }
--- lib/pages/qr_serach/qr_search.dart
+++ lib/pages/qr_serach/qr_search.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QRSearch extends StatefulWidget {
   QRSearch({Key? key}) : super(key: key);
@@ -8,10 +11,112 @@ class QRSearch extends StatefulWidget {
 }
 
 class _QRSearchState extends State<QRSearch> {
+  final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? _qrController;
+  int _goBackTimes = 0;
+  bool _cameraOnHold = false;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      _qrController!.pauseCamera();
+    } else if (Platform.isIOS) {
+      _qrController!.resumeCamera();
+    }
+  }
+
+  @override
+  void dispose() {
+    _qrController?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController qrController, BuildContext context) {
+    _qrController = qrController;
+    _qrController!.scannedDataStream.listen((scanData) {
+      _qrController!.pauseCamera();
+      _cameraOnHold = true;
+      _goBackTimes = 0;
+      ScaffoldMessenger.of(context).showMaterialBanner(
+        MaterialBanner(
+          leading: Icon(Icons.qr_code_rounded),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Text(
+                  "Contenido del QR:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text("${scanData.code}"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () async {
+                ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+                await Future.delayed(Duration(milliseconds: 600));
+                Navigator.of(context).pop();
+              },
+              child: Text("Salir"),
+            ),
+            TextButton(
+              onPressed: () async {
+                ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+                await Future.delayed(Duration(milliseconds: 600));
+                Navigator.of(context).pop("${scanData.code}");
+              },
+              child: Text("Buscar libro"),
+            )
+          ],
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Busqueda de libros con QR"),
+    return WillPopScope(
+      onWillPop: () async {
+        ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+        await _qrController!.resumeCamera();
+        _cameraOnHold = false;
+        _goBackTimes++;
+        // if (_goBackTimes == 1)
+        //   ScaffoldMessenger.of(context).showSnackBar(
+        //     SnackBar(
+        //       content: Text("Back de nuevo para salir"),
+        //     ),
+        //   );
+        // true: Navigator pop, false: return
+        return !_cameraOnHold && _goBackTimes > 1;
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: QRView(
+                overlay: QrScannerOverlayShape(
+                  borderWidth: 2,
+                  // cutOutHeight: 200,
+                  // cutOutWidth: 200,
+                ),
+                key: _qrKey,
+                onQRViewCreated: (qrController) {
+                  _onQRViewCreated(qrController, context);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
